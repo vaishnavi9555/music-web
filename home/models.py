990d24399ea76b13b03bad3d4bfa97c5d7bfd05b
khushi <vaishnavikumari673@gmail.com>
2023-10-29 20:07:12 +0530

    music

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Audio(models.Model):
+    file=models.FileField(upload_to='audio/')
+    user=models.ForeignKey(User , on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='thumbnail/',blank=True,null=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.file.url
+
--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from.models import Audio
 # Create your views here.
 def home(request):
+    ctx={'audiolist': Audio.objects.all()}
     return render(request, 'home.html', {})
+
+def upload(request):
+    form_files= request.FILES
+    ctx= {}
+    if form_files:
+        file = form_files['file']
+        image = form_files['image']
+        user= request.user or 1 # if no user,use user with id 1
+        Audio.objrcts.create(file=file,user=user,thumbnail=image)
+        ctx['success'] =True
+    else:
+        ctx['success'] = False
+        audios =Audio.objects.all()
+        ctx['audiolist']= audios
+        return render(request,'partials/audio_app.html', ctx)
